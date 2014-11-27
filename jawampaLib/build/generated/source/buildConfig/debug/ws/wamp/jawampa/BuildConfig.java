aee08fe6befb0a2849dbec38c4e797fac1034666
Nabil Souk <nabil@vinelab.com>
2014-11-27 16:39:52 +0200

    Created new Module: jawampa Lib (imported from Eclipse) Added the new module as a dependency for the project

---
+++ jawampaLib/build/generated/source/buildConfig/debug/ws/wamp/jawampa/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ws.wamp.jawampa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "ws.wamp.jawampa";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
