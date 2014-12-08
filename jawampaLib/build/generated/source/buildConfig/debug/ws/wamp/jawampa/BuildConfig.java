9bee4872a93f0408ad02c8e47d17bb13a131b2b9
Nabil Souk <nabil@vinelab.com>
2014-12-08 12:53:08 +0200

    Update gradle version after AndroidStudio update to version 1.0 RC 4

--- jawampaLib/build/generated/source/buildConfig/debug/ws/wamp/jawampa/BuildConfig.java
+++ jawampaLib/build/generated/source/buildConfig/debug/ws/wamp/jawampa/BuildConfig.java
@@ -5,7 +5,7 @@ package ws.wamp.jawampa;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "ws.wamp.jawampa";
+  public static final String APPLICATION_ID = "ws.wamp.jawampa";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
